446be082af43d0e5572a510c9d89bbe8a94e7a12
madhawan17 <aryamadhawan@gmail.com>
2025-09-28 16:30:04 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,17 @@
+#creating directory
+mkdir -p src
+mkdir -p research
+
+#Creating file 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt .py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirement.txt
+
+echo "Directory and files created successfully!."
+
+
