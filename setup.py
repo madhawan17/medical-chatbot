0be356869e69dd5fff89787195520aaad4f2dd25
madhawan17 <aryamadhawan@gmail.com>
2025-09-28 20:35:53 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AarogyaAi",
+    version="0.1.0",
+    author="Madhawan arya",
+    author_email="aryamadhawan@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
